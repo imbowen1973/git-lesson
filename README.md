ebc559b3a20f5faaf7d4ed289e1851923933751f
Mark.Bowen <mark.bowenvet@gmail.com>
2016-10-05 13:54:29 +0100

    Add scipt.r file to repository

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Git commit allows us to create a new revision and add this version
 
+Now added script.r to the repository
+
--- script.r
+++ script.r
@@ -1,2 +1,3 @@
 #This is the first document in R but this is just text comments
 #Wondering why etherpad isnt working
+#Added comments that are useful to the project
